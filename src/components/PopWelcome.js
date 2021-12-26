3fd93e6354b218f41531eae90542c754d102cb51
Yasin Kamış <74969496+yasinkamis@users.noreply.github.com>
2021-12-26 20:48:19 +0300

    bug fix

--- src/components/PopWelcome.js
+++ src/components/PopWelcome.js
@@ -8,6 +8,7 @@ const PopWelcome = () => {
             <h2>Window 11 Klonuna Hoş Geldin</h2>
             <h4>Şuan belli uygulamaların klonu olsa da zaman içinde artacak. Takipte kal !!!</h4>
             <h6>"BU POP UP I TEKRAR GÖRMEK İÇİN F5 TUŞUNA TIKLAMAN GEREK"</h6>
+            <h6>"Giriş yapma kısmı için localstorage'da ki logini silmelisiniz"</h6>
         </div>
     )
 }
